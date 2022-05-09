751df9d6c407edb7dd9122e1dc37e2256fd980d7
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-09 09:04:03 -0300

    js rules game

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const blue = document.querySelector('.blue');
 const yellow = document.querySelector('.yellow');
 const red = document.querySelector('.red');
 const green = document.querySelector('.green');
-
+//Função criar ordem aleatória
 let shuffleOrder = () =>{
     let colorOrder = Math.floor(Math.random()*4);
     order[order.length] = colorOrder;
@@ -19,7 +19,7 @@ let shuffleOrder = () =>{
         lightColor(elementColor, Number(i) + 1);
     }
 }
-
+//Função acende próxima cor
 let lightColor = (element, number) =>{
     number = number * 500;
     setTimeout(() =>{
@@ -29,15 +29,69 @@ let lightColor = (element, number) =>{
         element.classList.remove('selected');
     });
 }
-
+//Função checar click
 let checkOrder = () =>{
     for (let i in checkOrder) {
         if(clickOrder[i] != order[i]){
-            lose();
+            gameOver();
             break;
         }
     }
     if(clickOrder.length == order.length){
-        alert(`Pontuação: ${score}\n Você acertou, iniciando próximo nível!`)
+        alert(`Pontuação: ${score}\n Você acertou, iniciando próximo nível!`);
+        nextLevel();
     }
 }
+
+//Função click user
+let click = (color) =>{
+    clickOrder[clickOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() =>{
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    },250)
+    
+}
+
+//Função que retorna a cor
+let createColorElement = (color) =>{
+    if(color == 0){
+        return green;
+    }else if(color == 1){
+        return red;
+    }else if(color == 2){
+        return yellow;
+    }else if(color == 3){
+        return blue;
+    };
+}
+
+//Função próximo nível
+let nextLevel = () =>{
+    score++;
+    shuffleOrder();
+}
+
+//Função game over
+let gameOver = () =>{
+    alert(`Pontuação: ${score}\n Você perdeu o jogo!\n Clique em ok para reiniciar`);
+    order = [];
+    clickOrder = [];
+    playGame();
+}
+
+//Função iniciar jogo
+let playGame = () =>{
+    alert(`Bem vindo ao Game Genius, iniciando!`);
+    score = 0;
+    nextLevel();
+}
+
+green.addEventListener('click', click(0));
+red.addEventListener('click', click(1));
+yellow.addEventListener('click', click(2));
+blue.addEventListener('click', click(3));
+
+playGame();
